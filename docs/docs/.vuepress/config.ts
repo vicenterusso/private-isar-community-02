610424480b585f2c9e3b9e2b143c9dea394a0e68
Vicente Russo <vicente.russo@gmail.com>
2024-03-23 00:29:50 -0300

    Adds own analytics IDs

--- docs/docs/.vuepress/config.ts
+++ docs/docs/.vuepress/config.ts
@@ -133,7 +133,7 @@ export default defineUserConfig({
             "script",
             {
                 async: "",
-                src: "https://www.googletagmanager.com/gtag/js?id=G-NX9QJRWFGX",
+                src: "https://www.googletagmanager.com/gtag/js?id=G-36LNDL9RHB",
             },
         ],
         [
@@ -142,7 +142,7 @@ export default defineUserConfig({
             `window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-NX9QJRWFGX');`,
+                gtag('config', 'G-36LNDL9RHB');`,
         ],
         [
             "script",
@@ -151,7 +151,7 @@ export default defineUserConfig({
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "ciawnmxjdh");`,
+          })(window, document, "clarity", "script", "lkyzg3xacc");`,
         ],
     ],
 })
